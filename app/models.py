26fa19b144c9d0436d947dfd2dbc85f6a3e81da2
Curtis Heater <hotrun123@gmail.com>
2020-04-13 23:49:56 -0400

    First Commit, combined with getting to a point of almost working

---
+++ app/models.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+from app import db, login
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+#Table for user info such as contact info password and username
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+#Table to link Users and the cards they are watching
+#class UserCard(db.Model):
+ #   userId = db.Column(db.Integer, db.ForeignKey('user.id'))
+  #  cardId = db.Column(db.Integer, db.ForeignKey('card.id'))
+    
+
+#Card info such as version and name
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cardName = db.Column(db.String(32), index=True)
+    cardSet = db.Column(db.String(32), index=True)
+    
+    def __repr__(self):
+        return '<Card {}>'.format(self.cardName)
+
+#Webscrape results, will get info every 2 hours
+class Results(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Float)
+    cardId  = db.Column(db.Integer, db.ForeignKey('card.id'))
+    siteId = db.Column(db.Integer, db.ForeignKey('site.id'))
+
+    def __repr__(self):
+        return '<Search {}>'.format(self.price)
+
+#Sites for webscraps, used to show where prices come from
+class Site(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    siteLink = db.Column(db.String(32), unique=True, index=True)
+    siteName = db.Column(db.String(32), unique=True, index=True)
+
+    def __repr__(self):
+        return self.siteLink
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
